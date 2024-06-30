6f40aaf80bf46e0897d92fe7540e1d3c8929c10c
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2024-06-30 11:50:19 +0530

    Add files via upload

---
+++ ccp2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class ccp2{
+    public static void main (String [ ] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        String a= sc.nextInt();
+        System.out.println(a);
+        if(int a%8=0){
+            System.out.println("1");
+        }else{
+            System.out.println("-1");
+        }
+    }
+}
